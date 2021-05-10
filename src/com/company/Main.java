119a4edb185308f164d5ff594d4dab406f4cead0
Van Khanh <223344nguyen@gmail.com>
2021-05-10 11:02:43 +0700

    Thread_1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.rmi.server.RMIClassLoader;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello!");
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.print("dảk ");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        for (int i = 0; i < 10; i++) {
+            System.out.print("bủh ");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
+
+
+
