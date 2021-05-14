51cbb7a8907fa655e10122ded616df6e98095d8f
Van Khanh <223344nguyen@gmail.com>
2021-05-14 10:29:08 +0700

    submitRunnable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,31 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        List<Future> listFuture = new ArrayList<Future>();
+
         for (int i = 1; i <= 10; i++) {
             MyRunnable myRunnable = new MyRunnable("Runnable " + i);
-            executorService.execute(myRunnable);
+            Future future = executorService.submit(myRunnable);
+            listFuture.add(future);
+        }
+
+        for (Future future : listFuture) {
+            try {
+                System.out.println(future.get());
+            } catch (ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
         executorService.shutdown();
     }
