04296f19ac0796adfd68fa25970028a2ea8fac50
Van Khanh <223344nguyen@gmail.com>
2021-05-17 11:14:28 +0700

    EmployeesAdd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -9,23 +10,16 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        List<Future<String>> listFuture = new ArrayList<Future<String>>();
-        for (int i = 1; i <= 10; i++) {
-            MyCallable myCallable = new MyCallable("Callable " + i);
-            Future<String> future = executorService.submit(myCallable);
-            listFuture.add(future);
+    public static void main(String[] args) {
+        File f = new File("d:\\");
+        String S = f.isDirectory() ? "Thu muc" : "Tap tin";
+        System.out.println("../f11.txt la: " + S);
+        String L[] = f.list();
+        System.out.println("Noi dung thu muc: ");
+        for (int i = 0; i < L.length; ++i){
+            File f2 = new File(L[i]);
+            System.out.println(L[i] + " " + (f2.isFile()? "Tap tin" : "Thu muc"));
         }
-        for (Future future : listFuture) {
-            try {
-                System.out.println(future.get() + " kết thúc");
-            } catch (ExecutionException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-        executorService.shutdown();
     }
 }
 
